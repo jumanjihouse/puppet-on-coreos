f7a69cd69a4b05b325b8b531aeb2af39eeced3ae
Paul Morgan <jumanjiman@gmail.com>
2018-03-17 15:11:00 +0000

    ci: move env vars into build scripts to decouple from CI engine

---
+++ script/publish.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -eEu
+set -o pipefail
+
+. ci/vars
+
+docker login -e "${mail}" -u "${user}" -p "${pass}"
+
+# Pessimistic tag.
+docker tag puppet "jumanjiman/puppet:${TAG}"
+docker push "jumanjiman/puppet:${TAG}"
+
+# Optimistic tag.
+docker tag puppet jumanjiman/puppet:latest
+docker push jumanjiman/puppet:latest
+
+docker logout
+
+# Update https://microbadger.com/images/jumanjiman/puppet
+#
+curl -X POST 'https://hooks.microbadger.com/images/jumanjiman/puppet/28hXfM14w5i9q_1-yusNB2y5jLQ='
